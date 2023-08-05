5d7ccea788bc8ba57a1b55707d89283c1943f11c
lrurif <1984811813@qq.com>
2023-08-05 14:43:39 +0800

    feat: 简单集成vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from "./router/index"
+const vm =  createApp(App)
+vm.use(router);
+vm.mount('#app')
