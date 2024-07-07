3925f21b978d1c52c9c212f9b86e08a3b292006e
fangjm1143 <fangjm1143@cairenhui.com>
2024-07-07 14:46:25 +0800

    feat: 图标

--- packages/form-generate/main.js
+++ packages/form-generate/main.js
@@ -1,7 +1,6 @@
 import FormDesign from "./src/index.vue"
 import { installComs } from "@m-form/core"
 export default (app) => {
-    debugger;
     app.use(installComs)
     app.component("FormDesign", FormDesign);
 }
